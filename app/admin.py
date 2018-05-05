75595626c780b8dd5bc7dd9d7ae598c17a9c2bf9
tengjun <2357981239@qq.com>
2018-05-05 21:53:59 +0800

    0504eveing

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from .models import Post, Category, Entry
 # Register your models here.
+
+
+class PostAdmin(admin.ModelAdmin):
+    fields = ['name','created','published']
+
+
+admin.site.register(Post, PostAdmin)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
--- app/models.py
+++ app/models.py
@@ -1,29 +1,41 @@
 from django.db import models
 
 # Create your models here.
-class Post(models.Model):
-    name=models.CharField(max_length=100)
-    category=models.ManyToManyField(max_length=100)
-    created=models.DateTimeField(auto_now=True)
-    published=models.DateTimeField(auto_now_add==True)
 
-    is_published=models.BooleanField(default=False)
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class PostManager(models.Manager):
+    """
+    POST.objects
+    重载get_queryset,默认查询确认发布的文章
+    """
+
+    def get_queryset(self):
+        return super().get_queryset().filter(is_published=True)
+
+
+class Post(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField()
+    category = models.ManyToManyField(Category)
+
+    created = models.DateTimeField(auto_now=True)
+    published = models.DateTimeField(auto_now_add=True)
+
+    is_published = models.BooleanField(default=False)
+
+    objects = PostManager()
 
     def __str__(self):
         return self.name
 
 
 class Entry(models.Model):
-    post=models.OneToOneField(Post)
-    headline=models.CharField(max_length=100)
-    content=models.TextField()
+    post = models.OneToOneField(Post, on_delete=models.CASCADE)
+    content = models.TextField()
 
-    def __str__(self):
-        return self.headline
-
-class Category(models.CharField):
-    name=models.CharField(max_lenth=100)
 
 # User
-
-    
